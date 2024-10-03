257a85c512a2244c9a82f014362060fa10fea6c3
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-03 23:44:34 +0530

    Java Learning with Basics Concepts

---
+++ CWP_Ch1_PS.java
@@ -0,0 +1,28 @@
+package LearningJAVABasicsToAdvance;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class CWP_Ch1_PS {
+
+    public static void main(String[] args) {
+    /*    int a = 4;
+        int b = 7;
+        int c = 6;
+        int sum = a + b + c;
+        System.out.println(sum);*/
+
+
+       /* System.out.println("What is your name");
+        Scanner sc=new Scanner(System.in);
+        String name=sc.next();
+        System.out.println("Hello " + name +" Have a good day");
+*/
+
+        System.out.println("Enter your number: ");
+        Scanner input=new Scanner(System.in);
+        System.out.println(input.hasNextInt());
+
+
+    }
+}
+
---
+++ CWP_Literals04.java
@@ -0,0 +1,18 @@
+package LearningJAVABasicsToAdvance;
+
+public class CWP_Literals04 {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2=56;
+        long ageDino=566666666L;
+        short age3=87;
+        char ch='A';
+        float f1=5.6f;
+        double d1=4.66D;
+        boolean a=true;
+        String str="Priyanshi";
+        System.out.println(age);
+        System.out.println(str);
+        System.out.println();
+    }
+}
