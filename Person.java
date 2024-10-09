4ebac70b9c8dad737da2529399f06a10a73f5576
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-09 12:02:54 +0530

    Concrete classes Example

--- Person.java
+++ Person.java
@@ -1,4 +1,13 @@
 package LearningJAVABasicsToAdvance;
+//example of concrete class as well
+public class Person { //it can be public or not defined so that it will accessible to according to package
+    int empID;
 
-public class Person {
+    Person(int empID){
+        this.empID=empID;
+    }
+
+    public int getEmpID(){
+        return empID;
+    }
 }
