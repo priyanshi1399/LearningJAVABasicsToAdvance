001746facbe70f7a6625f829a379a95155aa399b
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-11 15:59:02 +0530

    2.StaticClassInheritedBySomeOtherClass

---
+++ OuterClass_StaticInheritenceNested.java
@@ -0,0 +1,13 @@
+package LearningJAVABasicsToAdvance;
+//2.Example of Static Inner Class inherited by some other class
+public class OuterClass_StaticInheritenceNested {
+
+    static class NestedClass{
+        public void display(){
+            System.out.println("Inside static Nested Class");
+        }
+    }
+
+}
+//this classes inherit a new Class check in someOtherClass
+
