2c67af3c7e73416e669b7d7eccb7a580c2c21182
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-11 15:54:07 +0530

    1.NestedClassInheritedByOtherClass(Inheritence by Nested)

--- ObjectTest2.java
+++ ObjectTest2.java
@@ -8,6 +8,15 @@ public static void main(String[] args){
 
     //static class can be accessed by OuterClass.display() suppose display method()
     //static means that is associated with class it is associated with OuterClass
+
+
+    //example of Inheritence in Nested Class creating Object
+    OuterClass_InheritenceNestedClass outerClassObj=new OuterClass_InheritenceNestedClass();
+    OuterClass_InheritenceNestedClass.InnerClass2  innerClass2Obj=outerClassObj.new InnerClass2();
+    innerClass2Obj.display();
+
+    //because it is nested class inside so first creating object of OuterClass then from that creating InnerClassObject
+    //then calling display method
 }
 
 }
