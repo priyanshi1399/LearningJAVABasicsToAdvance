17ea13b9f9412e2c098aa1d3b77131f20f302e80
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-11 15:13:22 +0530

    Understanding Object Classes by Example

--- ObjectTest.java
+++ ObjectTest.java
@@ -14,8 +14,26 @@ public class ObjectTest {
         System.out.println(obj2.getClass());// this will print obj2 is instance of which class
 
 
+
         //o/p-->class LearningJAVABasicsToAdvance.Person
         //class LearningJAVABasicsToAdvance.Audi
+
+         /* this 2 lines to understand static nested classes as well shown in Objecttest2 also
+        OuterClass.NestedClass  outernestedObj=new OuterClass.NestedClass();
+        outernestedObj.print();
+        */
+
+        /* this is static Inner class
+    OuterClass_InnerClasses outerClassObj=new OuterClass_InnerClasses(); //First we make Outer Object
+    OuterClass_InnerClasses.InnerClass innerClassOBj=outerClassObj.new InnerClass(); //by this outerObj make InnerClassObj
+    innerClassOBj.print(); //innerObject
+*/
+
+        //Invoking local Inner classes
+        OuterClass_LocalInnerClass outerLocalInnerObj=new OuterClass_LocalInnerClass();
+        outerLocalInnerObj.display(); //I have to call this display inside display it will create an object of localinnerclass and then print see in
+        //see in LocalInnerclass. //object will be created inside this class only.
+
     }
 }
 //Somewhere you want to store the object and you dont know which one is Parent and you want to hold the object
