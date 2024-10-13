721b7204f95901a875982be6bd33d438b94596c5
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-14 03:04:59 +0530

    GenericMethod

---
+++ GenericMethod.java
@@ -0,0 +1,13 @@
+package LearningJAVABasicsToAdvance;
+//Example of generic method
+public class GenericMethod { //this is not generic type
+
+    //supporting generic in method
+    public <K,V> void printValue(Generic_MoreThanOne<K,V> pair1, Generic_MoreThanOne<K,V> pair2){
+
+        if(pair2.getKey().equals(pair2.getKey())){
+            // do somwthing
+        }
+//before return type if you have to define the parameter
+    }
+}
