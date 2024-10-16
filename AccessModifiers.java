dd813912a29fe1ae8ecd60342f651f0b49403b1b
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-16 21:56:01 +0530

    AccessModifiersBasic

---
+++ AccessModifiers.java
@@ -0,0 +1,16 @@
+package LearningJAVABasicsToAdvance;
+
+public class AccessModifiers {
+    private int x;
+    int y;
+    protected int z;
+
+    public static  void main(String [] args){
+        AccessModifiers a=new AccessModifiers();
+        a.x=10;
+        //check in other classes as well.
+    }
+
+}
+
+
