0fdc8351a5d714e77d9e0a275d5ddbce721b5b89
priyanshi1399 <priyanshitomar001@gmail.com>
2025-04-23 20:01:11 +0530

    Anonymous Object

---
+++ Example_Any.java
@@ -0,0 +1,16 @@
+package LearningJAVABasicsToAdvance;
+
+class A7{
+    public A7(){
+        System.out.println("Object Created");
+    }
+
+}
+public class Example_Any {
+
+    public static void main(String [] args){
+        //A7 obj=new A7();
+        new A7(); //craeted in heap ,can't reuse them
+
+    }
+}
